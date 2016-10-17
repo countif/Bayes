3c1dba7c2063d7577ed715985dc18f3abcd45f75
labibfawaz <la_bib@hotmail.com>
2016-10-17 17:05:44 -0400

    Code EM without testing

--- HW2/ExpectationMaximization.py
+++ HW2/ExpectationMaximization.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from scipy.special import gammaln
 from scipy.stats import norm
 import math
 
@@ -17,34 +16,68 @@ class Probit():
         d = X_shape[1]
         w = np.zeros((d))
         e_phi = np.zeros((N,d))
+        self.logLikelihoods = []
+        K = set(self.y)
+
 
     def fit(self):
-        
-    def _eStep(self):
-        
+    
         def _var(x,w):
             return (-x.T*w)/self.sgm
-            
-        for i in range(N):
-            
-            _varValue = _variable(X[i],w)
-            
-            if y[i] == 1:
-                e_phi[i] = _varValue + (sgm * (norm.pdf(_varValue) / (1 - norm.cdf(_varValue))))
-            
-            else:
-                e_phi[i] = _varValue + (sgm * (-norm.pdf(_varValue) / norm.cdf(_varValue)))
-                
-    
-    def _mStep(self):
-        w = np.linalg.inv((lmbd + X.dot(X.T).sum())/sgm**2) * (X.dot(e_phi)/sgm**2)
         
-    def _logLikelihood(self):
+        def _eStep(self):
+            
+                
+            for i in range(N):
+                
+                _varValue = _var(X[i],w)
+                
+                if y[i] == 1:
+                    e_phi[i] = _varValue + (sgm * (norm.pdf(_varValue) / (1 - norm.cdf(_varValue))))
+                
+                else:
+                    e_phi[i] = _varValue + (sgm * (-norm.pdf(_varValue) / norm.cdf(_varValue)))
+                    
+        
+        def _mStep(self):
+            w = np.linalg.inv((lmbd + X.dot(X.T).sum())/sgm**2) * (X.dot(e_phi)/sgm**2)
+            
+        def _logLikelihood(self):
+            _varValue = _var(self.X,np.tile(w, N)
+
+            logLikelihood = (d/2 * np.log(lmbd/(2*math.pi))) - (lmbd/2*(w.T.dot(w))) + (self.y.dot(np.log(norm.cdf(_varValue)))).sum() + ((1 - self.y).dot(np.log(1 - norm.cdf(_varValue)))).sum()
+
+            return logLikelihood
+
+        for t in range(n_iter):
+            _eStep()
+            _mStep()
+            self.logLikelihoods.append(_logLikelihood())
+
+
 
         
 
     def predict_proba(self,Xtest):
         
+        self.Xtest = np.asarray(Xtest)
+        _probit = np.zeros((len(Xtest),len(K)))
+        predict_proba = np.zeros((len(Xtest),len(K)))
+        
+        def _probitDistribution(self,_varValue,y):
+            np.power(norm.cdf(_varValue),y) * np.power((1 - norm.cdf(_varValue)),(1-y))
+        
+        for i in range(N):
+            _varValue = _var(self.Xtest[i],w)
+            
+            for k in K:
+                _probit[i][k] = _probitDistribution(_varValue,k)
+
+            for k in K:
+                predict_proba[i][k] = _probit[i][k]/np.sum(_probit[i])    
+
+        return predict_proba
+
 
     def predict(self,Xtest):
         predict_proba = self.predict_proba(Xtest)
